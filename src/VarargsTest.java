0037c0ec85d5a85bad6a7f49ff07fb874df987ee
Serhiy17 <sd.workbox@gmail.com>
2015-12-29 00:02:23 +0200

    28.12.2015

---
+++ src/VarargsTest.java
@@ -0,0 +1,30 @@
+package src;
+
+public class VarargsTest {
+    public static double average(double... number) {
+
+        double total = 0.0;
+
+        for(double d : number)
+            total += d;
+
+        return  total/number.length;
+    }
+
+    public static void main(String[] args) {
+
+        double d1 = 10.0;
+        double d2 = 20.5;
+        double d3 = 30.8;
+        double d4 = 40.0;
+
+        System.out.printf("d1 = %.1f\nd2 = %.1f\nd3 = %.1f\nd4 = %.1f\n\n",
+                d1, d2, d3, d4);
+        System.out.printf("Average of d1 and d2 is %.1f\n",
+                average(d1, d2));
+        System.out.printf("Average of d1, d2 and d3 is %.1f\n",
+                average(d1, d2, d3));
+        System.out.printf("Average of d1, d2, d3 and d4 is %.1f\n",
+                average(d1, d2, d3, d4));
+    }
+}
